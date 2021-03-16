5aca55c081c5f4ad97f50992b8f7d339d2e6e213
drogowska <daria.rogo58@gmail.com>
2021-03-16 18:24:13 +0100

    add fixOneError method

--- src/error/correction/Algorithm.java
+++ src/error/correction/Algorithm.java
@@ -5,9 +5,11 @@ import java.util.Vector;
 
 public class Algorithm {
 
-    private static int[][] H = {    //Macierz H ma nie posiadać kolumny zerowej,
-            //identycznych kolumn oraz
-            //żadna z kolumn nie może być sumą dwóch innych
+    //Dmin = 4 dla korekcji 1 błędu ale detekcji 2, a Dmin= 5 dla korekcji 2 błędów
+
+    private static int[][] H = {    //Macierz H (parzystości) ma nie posiadać kolumny zerowej,
+                                    //identycznych kolumn oraz
+                                    //żadna z kolumn nie może być sumą dwóch innych
             {0,1,1,1,1,1,1,1,   1,0,0,0,0,0,0,0},
             {1,0,1,1,1,1,1,1,   0,1,0,0,0,0,0,0},
             {1,1,0,0,1,1,1,1,   0,0,1,0,0,0,0,0},
@@ -21,6 +23,12 @@ public class Algorithm {
     private int m = 8; //zmienna określająca długość wiadomości
     private int n = 8; //zmienna określająca długośc bitów parzystości
 
+    public void vert(byte[] list) {
+        List<Integer> tab = new Vector<>();
+        for(byte b : list) {
+           // tab.add(Integer.toBinaryString(Integer.bitCount()))
+        }
+    }
 
     //konwersja znaku ASCI do kodu binarnego zapisanego jako string
     private String AsciToBinaryString(char text) {
@@ -31,8 +39,6 @@ public class Algorithm {
         if(tmp.length()%8 != 0) {               //uzupełnienie 0 na początku do 8 bitów
             stringBuilder.append(tmp);
             stringBuilder.insert(0,'0');
-//            System.out.println("tu");
-//            System.out.println(stringBuilder.toString());
         } else {
             stringBuilder.append(tmp);
         }
@@ -72,10 +78,9 @@ public class Algorithm {
         char znak;
         for (int j=0;j<bin.size()/8;j++){
             znak = 0;
-//            for(i=j*8;i<8*(j+1);i++){
             for(int i=0;i<8;i++){
                 znak += bin.get(7+(j*8)-i) * Math.pow(2,i); // zamiana zer i jedynek na liczbę w zapisie dzisiętnym, która jest
-                // interpretowana jako znak z tablicy ASCI
+                                                            // interpretowana jako znak z tablicy ASCI
             }
             tab[j] = znak;
         }
@@ -84,17 +89,25 @@ public class Algorithm {
 
 
     // funkja wyknywująca mnożenie macierzy H przez wektor oraz dodająca bity parzystości
-    private void multiplyVectorByMatrixH(List<Integer> v) {
-        List<Integer> tmp = v;    //pomocnicza tablica przechowująca wynik operacji
+    private List<Integer> AddParityBites(List<Integer> v, int size) {
+        List<Integer> tmp = new Vector<>(v.size());    //pomocnicza tablica przechowująca wynik operacji
+        for(int i=0;i<v.size();i++) {
+            tmp.add(v.get(i));
+        }
+
         for (int i=0; i<n;i++) {
             int sum = 0;
-            for (int j = 0; j < v.size(); j++) {
-                sum += H[i][j] * tmp.get(i);
+            for (int j = 0; j <size; j++) {
+                //System.out.println(v.get(j));
+                sum += H[i][j] * v.get(j);            //wiersz H * kolumna v
             }
-            sum %= 2;           //operacje bitowe są wykonywane modulo 2, aby otrzyma otrzymać tylko 0 lub 1
-            v.add(sum);         //dodaje bit parzystości na koniec wiadomości
+            sum %= 2;             //operacje bitowe są wykonywane modulo 2, aby otrzyma otrzymać tylko 0 lub 1
+            tmp.add(sum);         //dodaje bit parzystości na koniec wiadomości
         }
-        //return tmp;
+//        List<Integer> res = multiply(v,tmp);
+//        System.out.println("rs" +res);
+//        return res;
+        return tmp;
     }
 
     //zakodowanie wiadomości, dodaje bity parzystości, zwraca vektor dwa razy dłuższy niż wektor podany jako parametr
@@ -105,8 +118,9 @@ public class Algorithm {
             tmp.add(text.get(i));
             copy.add(text.get(i));
             if( (i+1) % 8 == 0 && i != 0) {      //wstawienie po każdej literze (1 bajt) bitów parzystości
-                multiplyVectorByMatrixH(tmp);
-                System.out.println(tmp);
+                tmp = AddParityBites(tmp, n);
+//                System.out.println("tmp"+ tmp);
+//                System.out.println("copy"+copy);
                 for(int j=0;j<m;j++) copy.add(tmp.get(j+8));
                 tmp.clear();
             }
@@ -114,45 +128,92 @@ public class Algorithm {
         return copy;
     }
 
-    public void Correct(List<Integer> T) {
-        boolean error = false;      //zmienna określająca czy został znaleziony błąd czy nie
-        multiplyVectorByMatrixH(T);
-
-        int[] column = new int[n];
-        for(int i=0; i< T.size(); i++) {
-            if(T.get(i) != 0 ) error = true;
-        }
-    }
-
-
-
     //funkcja usuwa bity parzystości z podanego wektora
     public List<Integer> Decode(List<Integer> text) {
         List<Integer> list = new Vector<>();
-        int j = 0, n = 0;
+        int j = 0;
         System.out.println(text.size());
         while (j<text.size()){
             list.add(text.get(j));      //przepisanie do pomocniczej tablicy z pomienięciem bitów parzystości
-            if((j+1)%8==0){
+            if((j+1)%8 == 0){             //pominięcie 8 bitów po 8 bitach
                 j+=9;
-                n+=1;
             }
             else j+=1;
         }
         return  list;
     }
-    
-    private void FixOneErrors(List<Integer> HT, List<Integer> T){ //nie
+
+    //metoda znajdująca i poprawiająca jeden błąd dla jednej wiadomości
+    private boolean FixOneErrors(List<Integer> HE, List<Integer> T){ //nie sprawdzałam czy działa
         List<Integer> column = new Vector<>();
+        int s = 0;
         for(int i=0;i<m+n;i++){
-            for(int j=0;j<n;j++){
-                column.add(H[j][i]);        //wektor zawierający kolumny macierzy H
-                if(column.get(j) == HT.get(j)){      //sprawdzenie czy iloczyn HT ma takie same kolumny co macierz H,
-                    T.set(i,(T.get(i) == 1? 0:1));      // jeżeli tak to znaleziono błąd i następuje negacja bitu
-                }
+            for(int j=0;j<n;j++) {
+                column.add(H[j][i]);                    //wektor zawierający kolumny macierzy H
+                if (column.get(j) == HE.get(j)) s += 1;         //zmienna pomocnicza zliczająca ilość takich samych elementach w kolumnach
+            }
+
+            if(s == 8){
+                T.set(i,(T.get(i) == 1? 0:1));      //jeżeli tak to znaleziono błąd i następuje negacja bitu
+                return true;
             }
             column.clear();
+            s = 0;
         }
+        return false;
+    }
+
+    //
+    private boolean FixTwoErrors(List<Integer> T){
+
+
+
+        return false;
+    }
+
+    public List<Integer> Correct(List<Integer> T) {
+        int error = 0;                  //zmienna określająca czy został znaleziony błąd czy nie
+        //System.out.println("T" + T);
+        List<Integer> tmp =  new Vector<>();  //przechowuje 8 bitów wiadmosci i 8 bitów parzystości
+        List<Integer> result = new Vector<>();
+
+        for(int n=0;n<T.size()/16;n++) {            //wykonuje się dla 8 bitów wiadomośći i 8 bitów parzystość
+            for(int i=0;i<16;i++) {
+                tmp.add(T.get(i+(n*16)));
+            }
+            List<Integer> HT = multiply(tmp, new Vector<Integer>()); //pomnożenie wektora tmp i macierzy H
+            //System.out.print("tmp"+tmp);
+            for (int value : HT) {
+                if (value != 0) error = 1;       //jeżeli macież H*T zawiera 1 to znaleziono błąd
+            }
+            if (error==1) {
+                FixOneErrors(HT, tmp);
+           }
+            else if(error == 2){
+                //FixTwoErrors(tmp);
+            }
+            for(int v : tmp) result.add(v);
+            tmp.clear();        //czyszczenie wektorów
+            HT.clear();
+            error = 0;
+        }
+        //System.out.println("");
+        return result;
+    }
+
+    public List<Integer> multiply(List<Integer> v, List<Integer> tmp){
+        //List<Integer> tmp = new Vector<>(v.size());    //pomocnicza tablica przechowująca wynik operacji
+
+        for (int i=0; i<n;i++) {
+            int sum = 0;
+            for (int j = 0; j <n+m; j++) {
+                //System.out.println(v.get(j));
+                sum += H[i][j] * v.get(j);            //wiersz H * kolumna v
+            }
+            sum %= 2;             //operacje bitowe są wykonywane modulo 2, aby otrzyma otrzymać tylko 0 lub 1
+            tmp.add(sum);         //dodaje bit parzystości na koniec wiadomości
+        }
+        return tmp;
     }
 }
 
--- src/error/correction/Main.java
+++ src/error/correction/Main.java
@@ -1,8 +1,8 @@
 package error.correction;
 
-import error.correction.Algorithm;
-
+//import error.correction.Algorithm;
 import java.io.*;
+import java.util.List;
 import java.util.Vector;
 
 public class Main {
@@ -15,31 +15,53 @@ public class Main {
         String filePath = "./message.txt";
         String binaryFile = "./binary.txt";
         String encodedFile = "./encoded.txt";
+        String correctedFile = "./corrected.txt";
 
         BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
         String message = fileReader.readLine();         //odczyt z pliku wiadomości
         System.out.println(message);
+//        byte[] t = message.getBytes();
+//        for(byte i : t)
+//        {
+//            System.out.println("binary " + i );
+//            System.out.println("a " + Integer.toBinaryString(i));
+//        }
+
 
 
         PrintWriter BinWriter = new PrintWriter(binaryFile);
         //writer.println(algorithm.AsciToBinaryString(message));      //zapis do pliku binarnego zapisu wiadomości (string)
         BinWriter.print(algorithm.convertCharsToBinaryString(message));
-        System.out.println(algorithm.convertCharsToBinaryString(message));
+//        System.out.println(algorithm.convertCharsToBinaryString(message));
         BinWriter.close();
 
         PrintWriter EncWriter = new PrintWriter(encodedFile);
         Vector<Integer> V = (Vector<Integer>) algorithm.BinaryStringToBinary(algorithm.convertCharsToBinaryString(message));
-        System.out.println(V);
-        System.out.println(algorithm.BinaryToAsci(V));
-
-        System.out.println(algorithm.BinaryToBinaryString(algorithm.Encode(V)));
+//        System.out.println(V);
+//        System.out.println(algorithm.BinaryToAsci(V));
+//
+//        System.out.println(algorithm.BinaryToBinaryString(algorithm.Encode(V)));
         EncWriter.print(algorithm.BinaryToBinaryString(algorithm.Encode(V)));
         EncWriter.close();
         System.out.println("encode "+algorithm.Encode(V));
 
-        System.out.println("decode " + algorithm.Decode(algorithm.Encode(V)));
-        System.out.println(algorithm.BinaryToAsci(algorithm.Decode(algorithm.Encode(V))));
+        List<Integer> en = algorithm.Encode(V);
+        //System.out.println(en.equals(algorithm.Encode(V)));
+        System.out.println("decode " + algorithm.Decode(en));
+        System.out.println(algorithm.BinaryToAsci(algorithm.Decode(en)));
 
+        List<Integer> v1 = en;
+        System.out.println("V" + en);
+        System.out.println("v1" + v1);
+        v1.set(18,0);
+        v1.set(19,1);
+        System.out.println("v1" + v1);
+        //algorithm.Correct(v1);
+
+        PrintWriter corWriter = new PrintWriter(correctedFile);
+        System.out.print("");
+        System.out.println("correct" + algorithm.Correct(v1));
+        corWriter.write(algorithm.BinaryToBinaryString(v1));
 
 
     }
